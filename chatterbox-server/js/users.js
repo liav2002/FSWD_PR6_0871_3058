02cbd6916d5985bd8e9128e4bcf6771e710dd234
liav2002 <liavmail00@gmail.com>
2024-08-15 11:38:23 +0300

    getting other users info except logged in user.

--- chatterbox-server/js/users.js
+++ chatterbox-server/js/users.js
@@ -7,19 +7,19 @@ module.exports = (connection) => {
 
     // Login
     router.get('/loginUser', (req, res) => {
-        console.log("DEBUG: router '/loginUser' handler.");
+        console.log("SERVER-DEBUG: router '/loginUser' handler.");
 
         const phone = req.query.phone;
         const password = req.query.password;
         
-        console.log("DEBUG: request information:");
-        console.log("DEBUG: phone <- " + phone);
-        console.log("DEBUG: password <- " + password);
+        console.log("SERVER-DEBUG: request information:");
+        console.log("SERVER-DEBUG: phone <- " + phone);
+        console.log("SERVER-DEBUG: password <- " + password);
         
         // SQL query to retrieve the user's information with the corresponding phone and password
         connection.query('SELECT * FROM users WHERE phone = ? AND password = ?', [phone, password], (err, rows) => {
             if (err) {
-                console.error('ERROR: Failed executing the query:', err);
+                console.error('SERVER-ERROR: Failed executing the query:', err);
                 res.status(500).send('Failed retrieving user information');
             } 
             
@@ -47,24 +47,24 @@ module.exports = (connection) => {
 
     // Register
     router.post('registerUser', (req, res) => {
-        console.log("DEBUG: router '/registerUser' handler.");
+        console.log("SERVER-DEBUG: router '/registerUser' handler.");
 
         const user = req.body; // Retrieve user data from the request
 
-        console.log("DEBUG: request body:");
-        console.log("DEBUG: user detalis:");
+        console.log("SERVER-DEBUG: request body:");
+        console.log("SERVER-DEBUG: user detalis:");
         console.log(user);
 
         // Check if the phone number is already in use
         connection.query('SELECT * FROM users WHERE phone = ?', [user.phone], (err, rows) => {
             if (err) {
               console.error('Error while executing the query: ', err);
-              res.status(500).send('ERROR: Failed checking phone');
+              res.status(500).send('SERVER-ERROR: Failed checking phone');
             } 
             
             else {
               if (rows.length > 0) {
-                console.log("DEBUG: rows", rows);
+                console.log("SERVER-DEBUG: rows", rows);
                 res.status(400).send('Phone number is already in use');
               } 
               
@@ -74,7 +74,7 @@ module.exports = (connection) => {
                   [user.name, user.phone, user.email, user.profilePictureOption, user.status, user.password],
                   (err, result) => {
                     if (err) {
-                      console.error('ERROR: Failed while inserting user into the database: ', err);
+                      console.error('SERVER-ERROR: Failed while inserting user into the database: ', err);
                       res.status(500).send('Failed inserting user into the database');
                     } else {
                       const userToAdd = {
@@ -97,10 +97,10 @@ module.exports = (connection) => {
 
     // User Information
     router.get('/UserInfo', (req, res) => {
-        console.log("DEBUG: router '/UserInfo' handler.");
+        console.log("SERVER-DEBUG: router '/UserInfo' handler.");
 
         const userId = req.query.UserId;
-        console.log("DEBUG: user_id <- " + userId);
+        console.log("SERVER-DEBUG: user_id <- " + userId);
         
         // Create an SQL query with a prepared parameter
         const query = 'SELECT * FROM users WHERE id = ?';
@@ -108,14 +108,14 @@ module.exports = (connection) => {
         // Execute the SQL query with the parameter
         connection.query(query, [userId], (err, results) => {
           if (err) {
-            console.error('ERROR: Failed in request execution', err);
+            console.error('SERVER-ERROR: Failed in request execution', err);
             res.status(500);
             return res.send({ error: 'An error occurred while retrieving user details.' });
           }
       
           // If the query executed successfully without any errors
           const user = results[0]; 
-          console.log("DEBUG: user information:");
+          console.log("SERVER-DEBUG: user information:");
           console.log(user);
           res.json(user); // Send the new user as a response
         });
@@ -123,7 +123,7 @@ module.exports = (connection) => {
 
     // Update User Information
     router.put('/updateUserInfo', (req, res) => {
-        console.log("DEBUG: router '/updateUserInfo' handler.");
+        console.log("SERVER-DEBUG: router '/updateUserInfo' handler.");
 
         const userId = req.query.id; 
         const userName = req.query.name;
@@ -132,20 +132,20 @@ module.exports = (connection) => {
         const userEmail = req.query.email;
         const userProfile = req.query.profil;
 
-        console.log("DEBUG: request information:");
-        console.log("DEBUG: user_id <- " + userId);
-        console.log("DEBUG: user_name <- " + userName);
-        console.log("DEBUG: user_status <- " + userStatus);
-        console.log("DEBUG: user_password <- " + userPassword);
-        console.log("DEBUG: user_email <- " + userEmail);
-        console.log("DEBUG: user_profile <- " + userProfile);
+        console.log("SERVER-DEBUG: request information:");
+        console.log("SERVER-DEBUG: user_id <- " + userId);
+        console.log("SERVER-DEBUG: user_name <- " + userName);
+        console.log("SERVER-DEBUG: user_status <- " + userStatus);
+        console.log("SERVER-DEBUG: user_password <- " + userPassword);
+        console.log("SERVER-DEBUG: user_email <- " + userEmail);
+        console.log("SERVER-DEBUG: user_profile <- " + userProfile);
       
         const query = `UPDATE users SET name = ?, email = ?, profil = ?, status = ?, password = ? WHERE id = ?`;
       
         // Execute the SQL query with the parameters
         connection.query(query, [userName, userEmail, userProfile, userStatus, userPassword, userId], (error, results) => {
           if (error) {
-            console.error('ERROR: Failed in request execution', error);
+            console.error('SERVER-ERROR: Failed in request execution', error);
             res.status(500);
             return res.send({ error: 'An error occurred while updating the user profile.' });
           }
@@ -162,19 +162,19 @@ module.exports = (connection) => {
 
     // Information about chat group participants
     router.get('/ParticipantsInfo', (req, res) => {
-        console.log("DEBUG: router '/ParticipantsInfo' handler.");
+        console.log("SERVER-DEBUG: router '/ParticipantsInfo' handler.");
 
         const groupId = req.query.GroupId;
 
-        console.log("DEBUG: request information:");
-        console.log("DEBUG: group_id <- " + usergroupIdId);
+        console.log("SERVER-DEBUG: request information:");
+        console.log("SERVER-DEBUG: group_id <- " + usergroupIdId);
       
         const query = 'SELECT participantsId FROM chat_groups WHERE id = ?';
       
         // Execute the SQL query with the parameter
         connection.query(query, [groupId], (err, results) => {
           if (err) {
-            console.error('ERROR: Failed in request execution', err);
+            console.error('SERVER-ERROR: Failed in request execution', err);
             res.status(500);
             return res.send({ error: 'An error occurred while retrieving participants information.' });
           }
@@ -183,13 +183,32 @@ module.exports = (connection) => {
           const participantsIdArray = JSON.parse(group.participantsId);
           const participantsIdIntegers = participantsIdArray.map(id => parseInt(id));
       
-          console.log("DEBUG: participantsId as integers:");
+          console.log("SERVER-DEBUG: participantsId as integers:");
           console.log(participantsIdIntegers);
           res.json({ participantsId: participantsIdIntegers });
         });
     });
 
+    // Route GET to retrieve all users except the currently logged-in user
+    router.get('/AllUsers', (req, res) => {
+        console.log("SERVER-DEBUG: router '/AllUsers' handler.");
 
+        const loggedInUsername = req.query.loggedInUsername;
+
+        console.log("SERVER-DEBUG: request information:");
+        console.log("SERVER-DEBUG: Logged-In-Username <- " + loggedInUsername);
+
+        connection.query('SELECT * FROM users WHERE name != ?', [loggedInUsername], (err, rows) => {
+            if (err) {
+                console.error('SERVER-ERROR: Failed executing the query:', err);
+                res.status(500).send('Error retrieving users');
+            } 
+            
+            else {
+                res.json(rows);
+            }
+        });
+    });
     
     return router;
 };
